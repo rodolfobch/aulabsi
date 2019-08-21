c8a8a6832c05a5df6853126ce12b7dd0969414ed
Marcos <marcosfelipe@univem.edu.br>
2019-08-20 22:27:54 -0300

    btn ok - ola mundo

--- src/testegit/CadastroCliente.java
+++ src/testegit/CadastroCliente.java
@@ -36,6 +36,11 @@ public class CadastroCliente extends javax.swing.JFrame {
         jLabel1.setText("Nome:");
 
         jButton1.setText("OK");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -67,6 +72,10 @@ public class CadastroCliente extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        System.out.println("Ola Mundo");
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
